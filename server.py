866f8a56b98b4c1a610cdf9b2bd7003643cd05c2
monsieurlx <leoixeu@hotmail.fr>
2026-01-23 23:15:54 +0100

    testing with http transport

--- server.py
+++ server.py
@@ -14,4 +14,5 @@ def stock_price(ticker: str) -> str:
 
 
 if __name__ == "__main__":
-    mcp.run(transport="stdio")
+    mcp.run(transport="http", host="127.0.0.1", port=8000)
+    # mcp.run(transport="stdio")
